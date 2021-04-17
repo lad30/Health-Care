538a2804ede0d687a717eb38f1d37450294fed07
lad30 <hrithvikladlp@gmail.com>
2021-04-17 17:24:35 +0530

    Add ag grid

---
+++ src/app/features/patient/pages/patient-list/patient-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-list',
+  templateUrl: './patient-list.component.html',
+  styleUrls: ['./patient-list.component.scss']
+})
+export class PatientListComponent implements OnInit {
+
+  gridApi;
+  gridColumnApi;
+  defaultColDef = {
+    flex: 1,
+    minWidth: 150,
+    editable: true,
+  };
+
+  columnDefs = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price'}
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.gridApi.sizeColumnsToFit();
+  }
+
+}
--- src/app/features/patient/patient.module.ts
+++ src/app/features/patient/patient.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { PatientRoutingModule } from './patient-routing.module';
 import { PatientComponent } from './patient.component';
-
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
 
 @NgModule({
-  declarations: [PatientComponent],
+  declarations: [
+    PatientComponent,
+    PatientListComponent
+  ],
   imports: [
     CommonModule,
-    PatientRoutingModule
+    PatientRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class PatientModule { }
